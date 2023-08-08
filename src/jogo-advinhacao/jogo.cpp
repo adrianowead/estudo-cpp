d455c3fbbefb58d829d22320ea4260ab3bbca3d5
Adriano Maciel <adriano_mail@hotmail.com>
2023-08-08 16:34:22 -0300

    aula 2

--- src/jogo-advinhacao/jogo.cpp
+++ src/jogo-advinhacao/jogo.cpp
@@ -7,4 +7,7 @@ int main () {
     cout << "*************************************" << endl;
     cout << "* Bem-Vindos ao jogo da advinhação! *" << endl;
     cout << "*************************************" << endl;
+
+    int numero_secreto = 42;
+    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
 }
