4c448484647254e1dd355b345152f6989c3eb483
Adriano Maciel <adriano_mail@hotmail.com>
2023-08-08 16:34:11 -0300

    aula 1

---
+++ src/jogo-advinhacao/jogo.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "*************************************" << endl;
+    cout << "* Bem-Vindos ao jogo da advinhação! *" << endl;
+    cout << "*************************************" << endl;
+}
