66ab9133769957131d3f7f53739e156a5dcab4a2
Adriano Maciel <adriano_mail@hotmail.com>
2023-08-09 15:39:37 -0300

    validar arquivo de palavras

--- src/jogo-forca/jogo.cpp
+++ src/jogo-forca/jogo.cpp
@@ -135,18 +135,28 @@ vector<string> ler_arquivo()
     ifstream arquivo;
     arquivo.open("palavras.txt");
 
-    int quantidade_palavras;
-    arquivo >> quantidade_palavras;
-
-    for (int i = 0; i < quantidade_palavras; i++)
+    if (arquivo.is_open())
     {
-        string palavra_lida;
-        arquivo >> palavra_lida;
+        int quantidade_palavras;
+        arquivo >> quantidade_palavras;
 
-        palavras_arquivo.push_back(palavra_lida);
+        for (int i = 0; i < quantidade_palavras; i++)
+        {
+            string palavra_lida;
+            arquivo >> palavra_lida;
+
+            palavras_arquivo.push_back(palavra_lida);
+        }
+
+        arquivo.close();
+
+        return palavras_arquivo;
     }
-
-    return palavras_arquivo;
+    else
+    {
+        cout << "Não foi possível acessar o banco de palavras!" << endl;
+        exit(1);
+    }
 }
 
 void sorteia_palavra()
