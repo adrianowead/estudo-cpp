215942ec599317ed12e7590b5e66bab405403c09
Adriano Maciel <adriano_mail@hotmail.com>
2023-08-09 12:54:07 -0300

    usando for com iteração

--- src/jogo-forca/jogo.cpp
+++ src/jogo-forca/jogo.cpp
@@ -7,8 +7,12 @@ const string PALAVRA_SECRETA = "MELANCIA";
 
 bool letra_existe(char chute)
 {
-    for(int i = 0; i < PALAVRA_SECRETA.size(); i++) {
-        return chute == PALAVRA_SECRETA[i];
+    for (char letra : PALAVRA_SECRETA)
+    {
+        if (chute == letra)
+        {
+            return true;
+        }
     }
 
     return false;
