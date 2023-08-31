e5256334c5dad0640f240dc83e12941db31bbb19
Adriano Maciel <adriano_mail@hotmail.com>
2023-08-31 19:24:55 -0300

    TDD, criando primeiro o teste antes da implementação

--- src/Leilao/Leilao/UnitTest/teste-avaliador.cpp
+++ src/Leilao/Leilao/UnitTest/teste-avaliador.cpp
@@ -88,3 +88,22 @@ TEST_CASE("Avaliador")
 		REQUIRE(2000 == maiores3Lances[2].recuperaValor());
 	}
 }
+
+TEST_CASE("Leilão não deve receber testes consecutivos do mesmo usuário")
+{
+	// arrange
+	Leilao leilao("Fiat 147 0km");
+
+	Usuario adriano("Adriano");
+
+	Lance primeiroLance(adriano, 1000);
+	Lance segundoLance(adriano, 2000);
+
+	// act
+	leilao.recebeLance(primeiroLance);
+	leilao.recebeLance(segundoLance);
+
+	// assert
+	REQUIRE(1 == leilao.recuperaLances().size());
+	REQUIRE(1000 == leilao.recuperaLances()[0].recuperaValor());
+}
