5890282347dbdb12e91d5b3960ed4e652ba31e00
Adriano Maciel <adriano_mail@hotmail.com>
2023-08-31 16:01:36 -0300

    Escrevento o primeiro teste com o Catch2

--- src/Leilao/Leilao/UnitTest/teste-avaliador.cpp
+++ src/Leilao/Leilao/UnitTest/teste-avaliador.cpp
@@ -1,3 +1,23 @@
 #define CATCH_CONFIG_MAIN
 #include "Catch.hpp"
+#include "Avaliador.hpp"
 
+TEST_CASE("Deve recuperar maior lance de leilão em ordem crescente") {
+	// Arrange - Given
+	Lance primeiroLance(Usuario("Adriano Maciel"), 1000);
+	Lance segundoLance(Usuario("Vivian"), 2000);
+
+	Leilao leilao("Item X qualquer");
+	leilao.recebeLance(primeiroLance);
+	leilao.recebeLance(segundoLance);
+
+	Avaliador leiloeiro;
+
+	// Act - When
+	leiloeiro.avalia(leilao);
+
+	// Assert - Then
+	float valorEsperado = 2000;
+
+	REQUIRE(valorEsperado == leiloeiro.recuperaMaiorValor());
+}
